102649320b812e0531f8c8591efa3e15bc47adf0
ppabis <piotr@pabis.eu>
2025-05-19 14:38:49 +0200

    Infrastructure

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -18,3 +18,20 @@ provider "registry.opentofu.org/hashicorp/aws" {
     "zh:ecc4ea0f659dab585e8328698d0bb343b80721e093b95af13769a4616c082fab",
   ]
 }
+
+provider "registry.opentofu.org/hashicorp/awscc" {
+  version     = "1.41.0"
+  constraints = ">= 0.15.0, >= 1.0.0"
+  hashes = [
+    "h1:lvUHsLBamZhxua3Mkb2ha6NTYIcpRwS0XoWwBGc5YjQ=",
+    "zh:a4ad1cc17ce8f7a0777eb231fdfadeca03ccd7080fc0da143ee8924b0ff7ef94",
+    "zh:b7555c156f723bc225e0b8b1bea44df1e778901a29990e02a6c5a0657aacc41c",
+    "zh:bacfe88e65dc7a352c84046192a7dc6412990d0dcd95ee23b3c68fcf8c25c256",
+    "zh:c13be3279282071fb78b7645ec23eb2362e6ab25c08f08c542598cfbcb591481",
+    "zh:d34f0b835356fad09dd56214219efc8db4b59b51310ecab238be9161015bcf8e",
+    "zh:d6aee515b6212db2bce8d3fc7fb8d6c86fdead6eca4de028ee42421baa6f242d",
+    "zh:de407d3355506cfbe5d79439662738c5724ba566d49a9b5ab18e2b0031ebf223",
+    "zh:df33f74e91851aa884a1631e87a1e01aa68c69bbe7dfa188adfb204d6f53daad",
+    "zh:f7d2f54485003b7dd6bc849beb99acf3681df5d0d3de85fede3a733a30fe1538",
+  ]
+}
---
+++ infrastructure.tf
@@ -0,0 +1,45 @@
+module "vpc" {
+  source  = "aws-ia/vpc/aws"
+  version = ">= 4.2.0"
+
+  name       = "image-builder-vpc"
+  cidr_block = "10.123.0.0/16"
+  az_count   = 2
+  subnets = {
+    public  = { netmask = 24 }
+    private = { netmask = 24 }
+  }
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+
+  name         = "instance-sg"
+  vpc_id       = module.vpc.vpc_attributes.id
+  description  = "Security group for Image Builder"
+  egress_rules = ["all-all"]
+}
+
+module "instance_profile" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "5.55.0"
+
+  trusted_role_services   = ["ec2.amazonaws.com"]
+  role_name               = "image-builder-role"
+  create_role             = true
+  create_instance_profile = true
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder",
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ]
+}
+
+resource "aws_imagebuilder_infrastructure_configuration" "infrastructure" {
+  instance_types                = ["t4g.small", "t4g.medium"]
+  security_group_ids            = [module.security_group.security_group_id]
+  instance_profile_name         = module.instance_profile.iam_instance_profile_name
+  subnet_id                     = values(module.vpc.public_subnet_attributes_by_az)[0].id
+  name                          = "my-pipeline-infra"
+  terminate_instance_on_failure = true
+}
