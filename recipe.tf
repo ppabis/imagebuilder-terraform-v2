b5e2754d7b69c81dd0d53bde97ba85fdc965aab2
ppabis <piotr@pabis.eu>
2025-05-19 14:34:48 +0200

    Formatting

--- recipe.tf
+++ recipe.tf
@@ -5,18 +5,20 @@ data "aws_imagebuilder_image" "latest" {
 }
 
 resource "aws_imagebuilder_image_recipe" "image_recipe" {
-  component { 
+  name         = "my-image-recipe"
+  parent_image = data.aws_imagebuilder_image.latest.build_version_arn
+  version      = "1.0.0"
+  description  = "This is a recipe that takes latest Amazon Linux 2023 and installs latest Docker, Nginx and configures it."
+
+  component {
     component_arn = aws_imagebuilder_component.docker_component.arn
   }
-  component { 
+
+  component {
     component_arn = aws_imagebuilder_component.nginx_component.arn
     parameter {
       name  = "port"
       value = "9001"
     }
   }
-  name         = "my-image-recipe"
-  parent_image = data.aws_imagebuilder_image.latest.build_version_arn
-  version      = "1.0.0"
-  description  = "This is a recipe that takes latest Amazon Linux 2023 and installs latest Docker, Nginx and configures it."
 }
